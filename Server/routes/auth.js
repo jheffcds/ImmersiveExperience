876500babfd076d0f1c213a0e104b1058a1b0a5f
jheffcds <jheffcds@gmail.com>
2025-05-30 18:28:52 +0200

    update middleware

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Scene = require('../models/Scene');

61838d223cca47caf0782707b4adcbf10bb25a61
jheffcds <jheffcds@gmail.com>
2025-06-06 11:44:34 +0200

    Update users.js

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-const authenticateToken = require('../middleware/authMiddleware');
+const authenticateToken = require('../middleware/authenticateToken');
 const upload = require('../middleware/uploadMiddleware');
 const User = require('../models/User');
 

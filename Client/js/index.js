e9af361b76bc7230adb77c546bee78c468b38785
jheffcds <jheffcds@gmail.com>
2025-06-10 16:35:15 +0200

    Update index.js

--- Client/js/index.js
+++ Client/js/index.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         scenes.forEach(scene => {
           const card = document.createElement('div');
-          card.className = 'project-card';
+          card.className = 'project-cards';
 
           const imagePath = scene.images?.[0] || '';
           const imageUrl = imagePath

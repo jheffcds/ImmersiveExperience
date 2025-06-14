25d88fd3eed7a6f09cca255c11aa2aafabc3f669
jheffcds <jheffcds@gmail.com>
2025-06-14 14:22:39 +0200

    Update checkout.js

--- Server/routes/checkout.js
+++ Server/routes/checkout.js
@@ -15,7 +15,7 @@ router.post('/', authenticateToken, async (req, res) => {
       return res.status(400).json({ message: 'No scene IDs provided.' });
     }
 
-    const user = await User.findById(userId).select('email purchasedScenes');
+    const user = await User.findById(userId);
     if (!user) {
       return res.status(404).json({ message: 'User not found.' });
     }

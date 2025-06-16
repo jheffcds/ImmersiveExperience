e343dc627971e45deb758776e77b072c7a96c305
jheffcds <jheffcds@gmail.com>
2025-06-16 11:40:59 +0200

    Update checkout.js

--- Server/routes/checkout.js
+++ Server/routes/checkout.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY); // Replace with real secret key
 const Scene = require('../models/Scene');
+const User = require('../models/User');
 const authMiddleware = require('../middleware/authenticateToken'); // Protect route if needed
 
 router.post('/checkout', authMiddleware, async (req, res) => {
